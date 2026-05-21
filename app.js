e6be4eca03e97b57bdfd75c2b0b937a3171aeee5
Md Arafat Hosen <djarafathd@gmail.com>
2026-05-21 18:36:58 +0600

    Add initial app.js for YouTube clone functionality
    Initial implementation of a YouTube clone with video loading and search functionality.

---
+++ app.js
@@ -0,0 +1,36 @@
+
+// ====================================
+// A Tube YouTube Clone
+// ====================================
+
+
+const API_KEY = "PASTE_YOUR_YOUTUBE_API_KEY_HERE";
+
+
+const videoGrid = document.getElementById("videoGrid");
+const searchBtn = document.getElementById("searchBtn");
+const searchInput = document.getElementById("searchInput");
+
+
+const modal = document.getElementById("videoModal");
+const videoFrame = document.getElementById("videoFrame");
+const closeModal = document.getElementById("closeModal");
+
+
+// =============================
+// Load Default Videos
+// =============================
+
+loadVideos("Trending videos");
+
+
+// =============================
+// Search Button
+// =============================
+
+searchBtn.addEventListener("click", () => {
+
+  const query = searchInput.value;
+
+  if(query !== ""){
+    loadVideos(query);
